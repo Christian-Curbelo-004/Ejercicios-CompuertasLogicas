923c5e1f99ed4107959204715f89e021217e0ae9
unknown <joaquin.bauza@correo.ucu.edu.uy>
2025-05-14 18:57:43 -0300

    anclando proyecto

---
+++ Library/Not.cs
@@ -0,0 +1,14 @@
+﻿namespace Library;
+
+
+public class Not : IGate
+{
+    public bool input1;
+    
+    public bool Output { get; set; }
+
+    public void SetInputs(bool input1)
+    {
+        input1 = input1;
+    }
+}
