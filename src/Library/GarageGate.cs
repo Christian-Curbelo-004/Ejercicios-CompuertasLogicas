94f09a1e4adc3c2f241833dfe8cd6ec7565092c9
unknown <joaquin.bauza@correo.ucu.edu.uy>
2025-05-19 14:23:59 -0300

    clase garage gate arreglada

--- src/Library/GarageGate.cs
+++ src/Library/GarageGate.cs
@@ -5,32 +5,36 @@ public class GarageGate
     public static bool Evaluate(bool a, bool b, bool c)
     {
         // Crear instancias de las compuertas lógicas
-        var notA = new NOTgate();
-        var notB = new NOTgate();
-        var and1 = new ANDgate();
-        var and2 = new ANDgate();
-        var and3 = new ANDgate();
-        var and4 = new ANDgate();
-        var or = new ORgate();
+        var notA = new NotGate(a);
+        var notB = new NotGate(b);
+        //var and1 = new AndGate(c,nb);
+        //var and2 = new AndGate();
+        var and3 = new AndGate(c,b);
+        //var and4 = new AndGate();
+        //var or = new OrGate(term1,term2);
 
         // Calcular NOT a y NOT b
-        bool na = notA.Evaluate(a);
-        bool nb = notB.Evaluate(b);
+        bool na = notA.Evaluate();
+        bool nb = notB.Evaluate();
 
         // Calcular c AND NOT b
-        bool cAndNb = and1.Evaluate(c, nb);
+        var and1 = new AndGate(c,nb);
+        bool cAndNb = and1.Evaluate();
 
         // Calcular (c AND NOT b) AND NOT a
-        bool term1 = and2.Evaluate(cAndNb, na);
+        var and2 = new AndGate(cAndNb, na);
+        bool term1 = and2.Evaluate();
 
         // Calcular c AND b
-        bool cAndB = and3.Evaluate(c, b);
+        bool cAndB = and3.Evaluate();
 
         // Calcular (c AND b) AND a
-        bool term2 = and4.Evaluate(cAndB, a);
+        var and4 = new AndGate(cAndB, a);
+        bool term2 = and4.Evaluate();
 
         // Calcular el resultado final: term1 OR term2
-        bool result = or.Evaluate(term1, term2);
+        var or = new OrGate(term1,term2);
+        bool result = or.Evaluate();
 
         return result;
     }
